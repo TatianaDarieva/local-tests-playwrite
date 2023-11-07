105a071f25223aed4f8e8c8eb7cac2d974f5be64
Tatiana <tatianadarieva@gmail.com>
2023-11-07 09:41:49 -0500

    Adding test "Sign in with not existing credentials"

--- tests/auth.spec.js
+++ tests/auth.spec.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test.describe('Authentication & Authorization', () => {
-  test.only('Sign in with existing credentials', async ({page}) => {
+  test('Sign in with existing credentials', async ({page}) => {
     await page.goto('https://coding.pasv.us/user/login')
 
     // await page.pause()
@@ -11,5 +11,18 @@ test.describe('Authentication & Authorization', () => {
     await page.locator('button[type="submit"]').click()
 
     await expect(page.locator('.ant-avatar-square')).toBeVisible()
+  })
+  test('Sign in with not existing credentials', async ({page}) => {
+    await page.goto('https://coding.pasv.us/user/login')
+
+    // await page.pause()
+
+    await page.locator('#normal_login_email').fill('tatianadarieva@gmail.com')
+    await page.locator('#normal_login_password').fill('cucumber')
+    await page.locator('button[type="submit"]').click()
+
+    const toast = page.locator('.ant-notification-notice-message')
+    await expect(toast).toBeVisible()
+    await expect(toast).toHaveText('User login. Fail')
   })
 })
